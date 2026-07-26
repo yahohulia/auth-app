e697e18bc75dc10e1964a6c31262eda80e901554
Yaroslav <yaroslavhohulia@gmail.com>
2026-07-26 17:07:05 +0300

    added base url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../stores/auth.js';
 
 const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '',
   withCredentials: true,
 });
 
